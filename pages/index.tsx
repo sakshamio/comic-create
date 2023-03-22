0c5577fb64be1cb1d07126dbbfb639f7af193003
Saksham Gupta <gupta.saksham98@gmail.com>
2023-03-22 04:10:53 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -164,8 +164,8 @@ const Home: NextPage = () => {
               </div>
               <div className="space-y-8 flex flex-col items-center justify-center max-w-xl mx-auto">
                 {generatedBios
-                  .substring(generatedBios.indexOf("1") + 3)
-                  .split("2.")
+                  //.substring(generatedBios.indexOf("1") + 3)
+                  //.split("2.")
                   .map((generatedBio) => {
                     return (
                       <div
